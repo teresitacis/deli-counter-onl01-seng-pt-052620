ca48522878cb4fb30636e764d931a13b5e231901
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 02:56:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,1 @@
 # Write your code here.
-array = ["apple"]
-def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
-end
-
-oxford_comma(array)
