5574f2994fd8aed305e84fbb8a7b54628287f33d
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 02:56:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def oxford_comma(array)
+
+end 
+  case array.length 
+when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end  
